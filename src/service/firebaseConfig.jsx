f3692e6f2107e68242c1488422a68b47f11bb373
Aditya-devop <adityashriwal@gmail.com>
2025-02-28 00:47:10 +0530

    updated FB data

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,16 +9,16 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBGENAItqsFfhDOo0TiWsim2JW-ue8AXQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: "vihara-tour-planner.firebaseapp.com",
   projectId: "vihara-tour-planner",
   storageBucket: "vihara-tour-planner.firebasestorage.app",
   messagingSenderId: "119256457905",
   appId: "1:119256457905:web:7eeb31eae0fcf372537890",
-  measurementId: "G-PC3WQLVKHL"
+  measurementId: "G-PC3WQLVKHL",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app)
+export const db = getFirestore(app);
 // const analytics = getAnalytics(app);
